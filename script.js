641e0a146d31925f9a3e7620ea4d2bdcd3d1a24c
NB-Kamoni <kamoninjoki@gmail.com>
2024-03-25 15:06:38 +0300

    Adds script.js and style.css files

---
+++ script.js
@@ -0,0 +1,7 @@
+// create a h2 element and add some text content to it
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+
+// add the h2 element to the body on the DOM
+document.querySelector("body").appendChild(h2);
+
